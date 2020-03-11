6a4f0e6b81e43da6c1b50677714f2990f4bc3128
tlcsanshao <qianyinqy@163.com>
2020-03-11 14:27:18 +0800

    remove a note

--- EurekaServer/src/main/java/com/sanshao/eurekaserver/SpringCloudTestApplication.java
+++ EurekaServer/src/main/java/com/sanshao/eurekaserver/SpringCloudTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringCloudTestApplication {
-//test abc
+//remove abc
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudTestApplication.class, args);
 	}
